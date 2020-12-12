e4a197409ac5046058b229772aaf8f0d09ccf570
vaibhavcs99 <vcsonawane99@gmail.com>
2020-12-12 16:07:22 +0530

    Trying out something, hope it works

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "kdfdsn",
+    home: GitLearn(),
+  ));
+}
+
+
+class GitLearn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("fnd"),
+      ),
+      body: Container(child:Text("jfdsfkds"))
+    );
+  }
+}
